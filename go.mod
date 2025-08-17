0a916c9d0b4029a327bc5707f89499cc5681665f
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2025-08-18 01:44:49 +0530

    cors policy added

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.25.0
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/joho/dotenv v2.2.0+incompatible // indirect
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/joho/dotenv v2.2.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -16,5 +19,26 @@ func main() {
 		log.Fatal("PORT is not found in the environment variables")
 	}
 	fmt.Println("Port: ", portString)
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	fmt.Printf("Starting Server at Port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
