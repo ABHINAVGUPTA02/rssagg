da8f9a571bdf1c75b8adfb072966b6acff4ff455
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2025-08-20 02:14:03 +0530

    db setup and users created

---
+++ models.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"time"
+
+	"github.com/ABHINAVGUPTA02/rssagg/internal/database"
+	"github.com/google/uuid"
+)
+
+type Users struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+}
+
+func databaseUserToUser(dbUser database.User) Users {
+	return Users{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+	}
+}
