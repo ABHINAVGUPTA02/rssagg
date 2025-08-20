07ca23b03543baa787f44a4ce488ccc9f09bd962
ABHINAVGUPTA02 <abhinav.gupta.dev05@gmail.com>
2025-08-20 16:06:23 +0530

    middleware auth added

--- handler_users.go
+++ handler_users.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/ABHINAVGUPTA02/rssagg/internal/auth"
 	"github.com/ABHINAVGUPTA02/rssagg/internal/database"
 	"github.com/google/uuid"
 )
@@ -36,18 +35,6 @@ func (apiCfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, 201, databaseUserToUser(user))
 }
 
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	key, err := auth.GetAPIKEY(r.Header)
-	if err != nil {
-		respondWithError(w, 403, fmt.Sprintf("Couldn't get API key: ", err))
-		return
-	}
-
-	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), key)
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: ", err))
-		return
-	}
-
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
